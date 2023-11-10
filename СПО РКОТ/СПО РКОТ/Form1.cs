e2b2d36e4e61401577760971cd1c45babd0bac85
yam501 <ya.misha501@gmail.com>
2023-11-10 15:21:21 +0500

    button test

--- СПО РКОТ/СПО РКОТ/Form1.cs
+++ СПО РКОТ/СПО РКОТ/Form1.cs
@@ -6,5 +6,15 @@ namespace СПО_РКОТ
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
