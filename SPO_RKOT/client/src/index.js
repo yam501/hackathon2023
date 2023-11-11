19b3252c3c7538bb18badc34e994f15d014814d3
orfimorf <ilya.lapushkin@mail.ru>
2023-11-11 13:08:23 +0500

    сервисы, сторы и контекс

--- SPO_RKOT/client/src/index.js
+++ SPO_RKOT/client/src/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import ExternalTableStore from './store/ExternalTableStore';
+import InternalTableStore from './store/InternalTableStore';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+export const Context = createContext()
+
+
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    externalTable: new ExternalTableStore(),
+    internalTable: new InternalTableStore()
+
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
 
 
---
+++ SPO_RKOT/client/src/service/InternalTableService.js
@@ -0,0 +1,66 @@
+import { $host } from "../http";
+
+
+const InternalTableService = {
+    async create(
+        externalTableId, companyName,
+
+        voiceServiceNonAcessibility, voiceServiceCutOffRatio, speechQualityonCallbasis, negativeMOSSamplesRatio,
+        undeliveredSMSRatio, averageSMSTime,
+
+        HTTPSessionFailureRatio, HTTPULMeanUserDataRate, HTTPDLMeanUserDataRate, HTTPSessionTime,
+
+        testVoiceConnectionQuantity, POLQA, negativeMOSSamplesCount, SMSQuantity, quantityConnection,
+        quantitySessions) {
+        return new Promise((resolve) => resolve($host.post('api/internalTable/create', {
+            externalTableId, companyName,
+
+            voiceServiceNonAcessibility, voiceServiceCutOffRatio, speechQualityonCallbasis, negativeMOSSamplesRatio,
+            undeliveredSMSRatio, averageSMSTime,
+
+            HTTPSessionFailureRatio, HTTPULMeanUserDataRate, HTTPDLMeanUserDataRate, HTTPSessionTime,
+
+            testVoiceConnectionQuantity, POLQA, negativeMOSSamplesCount, SMSQuantity, quantityConnection,
+            quantitySessions
+        })))
+    },
+
+    async fetchAll() {
+        return new Promise((resolve) => resolve($host.post('api/internalTable/getAll')))
+    },
+
+    async getAllByExternalTableId(externalTableId) {
+        return new Promise((resolve) => resolve($host.post('api/internalTable/getAllByExternalTableId', { externalTableId })))
+    },
+
+    async changeDataById(id,
+        externalTableId, companyName,
+
+        voiceServiceNonAcessibility, voiceServiceCutOffRatio, speechQualityonCallbasis, negativeMOSSamplesRatio,
+        undeliveredSMSRatio, averageSMSTime,
+
+        HTTPSessionFailureRatio, HTTPULMeanUserDataRate, HTTPDLMeanUserDataRate, HTTPSessionTime,
+
+        testVoiceConnectionQuantity, POLQA, negativeMOSSamplesCount, SMSQuantity, quantityConnection,
+        quantitySessions) {
+        return new Promise((resolve) => resolve($host.post('api/internalTable/changeDataById', {
+            id,
+            externalTableId, companyName,
+
+            voiceServiceNonAcessibility, voiceServiceCutOffRatio, speechQualityonCallbasis, negativeMOSSamplesRatio,
+            undeliveredSMSRatio, averageSMSTime,
+
+            HTTPSessionFailureRatio, HTTPULMeanUserDataRate, HTTPDLMeanUserDataRate, HTTPSessionTime,
+
+            testVoiceConnectionQuantity, POLQA, negativeMOSSamplesCount, SMSQuantity, quantityConnection,
+            quantitySessions
+        })))
+    },
+
+    async delete(id) {
+        return new Promise((resolve) => resolve($host.post('api/internalTable/delete', { id })))
+    }
+
+}
+
+export default InternalTableService;
