a7a67367b8f3e443b3ce930be5ffd2ca25f231be
yam501 <ya.misha501@gmail.com>
2023-11-11 12:56:57 +0500

    modal window information

--- SPO_RKOT/client/src/components/Header/Header.js
+++ SPO_RKOT/client/src/components/Header/Header.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ButtonUI from '../ButtonUI';
 import './header.css'
+import ModalInfo from "./ModalInfo";
+
 const Header = () => {
+
+    const  [show, setShow] = useState(false);
+
+    const showModal = () => {
+        setShow(!show)
+    }
+
     return (
         <div className='header'>
+            {show ?
+                <ModalInfo show={showModal}/>
+                :
+                <></>
+            }
+
             <div className='header_box_content'>
                 <div className='header_title'>
                     <span className='header_title_text'>СПО РКОТ</span>
                 </div>
                 <div className='header_btn_box'>
                     <ButtonUI className='header_btn'>Обновить</ButtonUI>
-                    <ButtonUI className='header_btn'>Информация</ButtonUI>
+                    <ButtonUI onClick={() => setShow(!show)} className='header_btn'>Информация</ButtonUI>
                 </div>
             </div>
         </div>
