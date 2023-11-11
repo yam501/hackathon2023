d0d255fb9c87d9a93ca3ae3a941df1d90b661b33
Danil <kraskafet@gmail.com>
2023-11-12 02:07:24 +0500

    второй столбец - вторая таблица

--- SPO_RKOT/client/src/components/Table/TableHead.js
+++ SPO_RKOT/client/src/components/Table/TableHead.js
@@ -6,11 +6,18 @@ import Col from 'react-bootstrap/Col';
 
 const TableHead = ({item, dataTable, index, ...props}) => {
     const [colValue, setColValue] = useState('');
-    
+    const [dictionary, setDictionary] = useState(
+        {
+            'voiceServiceNonAcessibility': 'Не более 5',
+            'voiceServiceCutOffRatio': 'Не более 5',
+            'speechQualityonCallbasis': 'Не менее 2.5',
+            'HTTPDLMeanUserDataRate': 'Не менее 80'
+        }
+    )
     return (
         <Row>
         <Col>{item}</Col>
-        <Col></Col>
+        <Col>{dictionary[item]}</Col>
         {dataTable.map(tableItem => <Col><input key={tableItem.id} value={colValue === '' ? tableItem[item] : colValue} onChange={e => setColValue(e.target.value)}/></Col>)}
     </Row>
     );
