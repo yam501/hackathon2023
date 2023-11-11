597d04cc74f82cca9bc364313f341e53273723f3
yam501 <ya.misha501@gmail.com>
2023-11-11 13:33:30 +0500

    delete mini krestik

--- SPO_RKOT/client/src/components/Header/ModalInfo.js
+++ SPO_RKOT/client/src/components/Header/ModalInfo.js
@@ -7,7 +7,7 @@ const ModalInfo = ({show}) => {
     return (
         <div onClick={show} className="modal">
             <div className="modal-content">
-                <span onClick={show} className="closeModal">&times;</span>
+                <span onClick={show} className="closeModal"></span>
                 <p>Кнопка «Обновить» предназначена для ручной, повторной или
                     принудительной инициализации подключения, а также получения данных из
                     БД</p>
