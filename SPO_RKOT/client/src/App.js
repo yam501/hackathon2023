b3cbec8ddf1cef0dfe433da6bd8b34b9e0093f2e
orfimorf <ilya.lapushkin@mail.ru>
2023-11-11 16:04:23 +0500

    second tabl creation

--- SPO_RKOT/client/src/App.js
+++ SPO_RKOT/client/src/App.js
@@ -14,6 +14,7 @@ var XLSX = require("xlsx");
 function App() {
 
   const { externalTable } = useContext(Context)
+  const { internalTable } = useContext(Context)
 
 
   const [openTable, setOpenTable] = useState(false)
@@ -32,7 +33,7 @@ function App() {
   
   const handleFile = async e => {
     const file = e.target.files[0]
-
+    if (!file.name.includes('.xlsx') || !file.name.includes('.xls')) return console.log('выбран не тот тип файлов')
     const data = await file.arrayBuffer();
     const workbook = XLSX.readFile(data, { sheetRows: 50 })
 
@@ -51,18 +52,58 @@ function App() {
     let place = jsonData[13][0]?.substring(27)
     let time = jsonData[12][0]?.substring(30)
     let exTab = await externalTable.create(district, place, time)
-
-
+    
+    await createInternalTable(jsonData, exTab.data.id)
   }
 
 
+  const createInternalTable = async (jsonData, exId) => {
+    const externalTableId = exId
+    const lenOfCompany = jsonData[17].length - 2
+    for(let i = 2; i<=lenOfCompany; i++ ){
+      let companyName = jsonData[17][i]
+      let voiceServiceNonAcessibility = jsonData[18][i]
+      let voiceServiceCutOffRatio = jsonData[19][i]
+      let speechQualityonCallbasis = jsonData[20][i]
+      let negativeMOSSamplesRatio = jsonData[21][i]
+
+      let undeliveredSMSRatio = jsonData[23][i]
+      let averageSMSTime = jsonData[24][i]
+
+
+      let HTTPSessionFailureRatio = jsonData[26][i]
+      let HTTPULMeanUserDataRate = jsonData[27][i]
+      let HTTPDLMeanUserDataRate = jsonData[28][i]
+      let HTTPSessionTime = jsonData[29][i]
+
+      let testVoiceConnectionQuantity = jsonData[31][i]
+      let POLQA = jsonData[32][i]
+      let negativeMOSSamplesCount = jsonData[33][i]
+      let SMSQuantity = jsonData[34][i]
+      let quantityConnection = jsonData[35][i]
+      let quantitySessions = jsonData[36][i]
+
+      await internalTable.create(externalTableId, companyName,
+
+        voiceServiceNonAcessibility, voiceServiceCutOffRatio, speechQualityonCallbasis, negativeMOSSamplesRatio,
+        undeliveredSMSRatio, averageSMSTime,
+
+        HTTPSessionFailureRatio, HTTPULMeanUserDataRate, HTTPDLMeanUserDataRate, HTTPSessionTime,
+
+        testVoiceConnectionQuantity, POLQA, negativeMOSSamplesCount, SMSQuantity, quantityConnection,
+        quantitySessions)
+    }
+
+  
+    console.log(lenOfCompany)
+  }
 
   return (
     <div className="App">
       <Header handleFile={handleFile} />
       { openTable && <InformPanel openTable={open}/>}
       <Table openTable={open} show={openTable} />
-
+      <button onClick={() => console.log(externalTable.externalTable)}> ЖМИ МЕНЯ</button>
 
     </div>
   );
